7b98ea112034417dc7c2c7c32953a7f218d08dbb
Timur Karimov <tationkar@gmail.com>
2023-06-20 16:04:09 +0300

    1st n 2nd

--- DZ9_task0/Program.cs
+++ DZ9_task0/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+using System;
+using static System.Console;
+Clear();
+Write("Введите целое число: ");
+int n = int.Parse(ReadLine()!);
+WriteLine(RecursionN(n));
+string RecursionN(int n){
+
+    if(n==1)
+        return "1";
+
+    string s = n.ToString() + ", " + RecursionN(n-1)  ;
+
+    return s;
+}
+
--- DZ9_task1/Program.cs
+++ DZ9_task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+using System;
+using static System.Console;
+Clear();
+Write("Введите целое число M: ");
+int m = int.Parse(ReadLine()!);
+Write("Введите целое число N: ");
+int n = int.Parse(ReadLine()!);
+int tempNum = m;
+if (n < m)
+{
+    m = n; n = tempNum;
+}
+WriteLine(RecursionMN(m, n));
+int RecursionMN(int m, int n)
+{
+    if (n < m)
+        return 0;
+    int res = m + RecursionMN(++m, n);
+    return res;
+}
